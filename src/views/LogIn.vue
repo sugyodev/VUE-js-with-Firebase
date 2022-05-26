86d8c1a454eab4ab7053271ba640db6754f39270
gshohat <gilad.shohat@gmail.com>
2022-05-26 19:43:25 +0100

    workflow unitest@add element plus

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,17 +1,45 @@
 <template>
   <main>
-    <section>
-      <button data-vi="login">Login</button>
-    </section>
+    <el-form class="auth">
+      <el-form-item label="email" data-vi="email">
+        <el-input v-model="form.email"/>
+      </el-form-item>
+
+      <el-form-item label="password" data-vi="password">
+        <el-input v-model="form.password"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onLogin" data-vi="login">Login</el-button>
+        <el-button type="primary" @click="onSignup"><slot><span data-vi="signup">Signup</span></slot></el-button>
+      </el-form-item>
+    </el-form>
   </main>
 </template>
 
-<script>
-export default {
-  name: 'LogIn',
-};
+<script setup>
+import { reactive } from 'vue';
+
+const form = reactive({
+  email: '',
+  password: '',
+})
+
+const emit = defineEmits()
+
+const onSignup = () => {
+  emit('signup', form)
+}
+
+const onLogin = () => {
+
+}
+
 </script>
 
 <style scoped>
+
+.auth {
+
+}
 
 </style>
