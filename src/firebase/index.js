900a81d3847b52e0fd3351fe98b0be6cd28ef3a9
gshohat <gilad.shohat@gmail.com>
2022-05-28 16:20:42 +0100

    - cypress@env variables

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-};
+    apiKey: Cypress.env('API_KEY'),
+    authDomain: Cypress.env('AUTH_DOMAIN'),
+    projectId: Cypress.env('PROJECT_ID'),
+    storageBucket: Cypress.env('process.env.STORAGE_BUCKET'),
+    messagingSenderId: Cypress.env('process.env.MESSAGING_SENDER_ID'),
+    appId: Cypress.env('APP_ID'),
+}
 
 const app = initializeApp(firebaseConfig);
 
