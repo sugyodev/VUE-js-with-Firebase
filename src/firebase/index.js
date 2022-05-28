49827ae4728613bd88bcf0550500416d673b2045
gshohat <gilad.shohat@gmail.com>
2022-05-28 18:56:25 +0100

    - env variables@different setup

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,15 @@ import { getAuth } from 'firebase/auth';
 
 let firebaseConfig = null
 
-if (typeof process === 'undefined') {
+console.log('import.meta.env', import.meta.env)
+console.log('-----------------')
+console.log('-----------------')
+console.log('-----------------')
+console.log('-----------------')
+console.log('-----------------')
+console.log('-----------------')
+
+if (typeof Cypress === 'object') {
     firebaseConfig = {
         apiKey: Cypress.env('API_KEY'),
         authDomain: Cypress.env('AUTH_DOMAIN'),
@@ -11,6 +19,15 @@ if (typeof process === 'undefined') {
         storageBucket: Cypress.env('process.env.STORAGE_BUCKET'),
         messagingSenderId: Cypress.env('process.env.MESSAGING_SENDER_ID'),
         appId: Cypress.env('APP_ID'),
+    }
+} else if (typeof import.meta.env === 'object') {
+    firebaseConfig = {
+        apiKey: import.meta.env.VITE_API_KEY,
+        authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_APP_ID,
     }
 } else {
     firebaseConfig = {
@@ -20,8 +37,7 @@ if (typeof process === 'undefined') {
         storageBucket: process.env.VITE_STORAGE_BUCKET,
         messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
         appId: process.env.VITE_APP_ID,
-    };
-
+    }
 }
 
 
