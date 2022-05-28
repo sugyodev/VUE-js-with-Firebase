280533378c1b06e551b5f95cdef294fcfca72f6c
gshohat <gilad.shohat@gmail.com>
2022-05-28 18:20:44 +0100

    - env variables@differentiate cypress vs vitest

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,29 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+let firebaseConfig = null
 
-const firebaseConfig = {
-    apiKey: Cypress.env('API_KEY'),
-    authDomain: Cypress.env('AUTH_DOMAIN'),
-    projectId: Cypress.env('PROJECT_ID'),
-    storageBucket: Cypress.env('process.env.STORAGE_BUCKET'),
-    messagingSenderId: Cypress.env('process.env.MESSAGING_SENDER_ID'),
-    appId: Cypress.env('APP_ID'),
+if (typeof process === 'undefined') {
+    firebaseConfig = {
+        apiKey: Cypress.env('API_KEY'),
+        authDomain: Cypress.env('AUTH_DOMAIN'),
+        projectId: Cypress.env('PROJECT_ID'),
+        storageBucket: Cypress.env('process.env.STORAGE_BUCKET'),
+        messagingSenderId: Cypress.env('process.env.MESSAGING_SENDER_ID'),
+        appId: Cypress.env('APP_ID'),
+    }
+} else {
+    firebaseConfig = {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+    };
+
 }
+
 
 const app = initializeApp(firebaseConfig);
 
